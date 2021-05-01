003b94f603225d08bcb5cb3eb6fe69a07a20c5fb
David Hotham <david.hotham@blueyonder.co.uk>
2021-05-01 19:54:29 +0100

    initial commit

---
+++ puzzle.py
@@ -0,0 +1,93 @@
+#!/bin/env python
+from ortools.sat.python import cp_model
+
+
+def solve_puzzle():
+    """Solve a puzzle"""
+    # Prepare a model.
+    model = cp_model.CpModel()
+
+    # Create variables.
+    [canaries, spain, switzerland, czech_republic, bulgaria] = range(5)
+
+    nests = [model.NewIntVar(0, 4, "nests{i}".format(i=i)) for i in range(5)]
+    [chimney, hedge, house, lake, straw] = nests
+
+    breeds = [model.NewIntVar(0, 4, "breeds{i}".format(i=i)) for i in range(5)]
+    [finch, mallard, starling, swallow, woodpecker] = breeds
+
+    foods = [model.NewIntVar(0, 4, "foods{i}".format(i=i)) for i in range(5)]
+    [bread, cake, cookies, croissants, scones] = foods
+
+    activities = [model.NewIntVar(0, 4, "activities{i}".format(i=i)) for i in range(5)]
+    [bird_baths, collecting, pecking, stealing, squawking] = activities
+
+    hometowns = [model.NewIntVar(0, 4, "hometowns{i}".format(i=i)) for i in range(5)]
+    [barking, camden, ealing, hounslow, kensington] = hometowns
+
+    model.AddAllDifferent(nests)
+    model.AddAllDifferent(breeds)
+    model.AddAllDifferent(foods)
+    model.AddAllDifferent(activities)
+    model.AddAllDifferent(hometowns)
+
+    # collecting-twigs next to kensington
+    diff_collecting_kensington = model.NewIntVar(-4, 4, "diff_collecting_kensington")
+    model.Add(diff_collecting_kensington == collecting - kensington)
+    model.AddAbsEquality(1, diff_collecting_kensington)
+
+    # bird-baths next to brentford (which is in hounslow)
+    diff_birdbaths_hounslow = model.NewIntVar(-4, 4, "diff_birdbaths_hounslow")
+    model.Add(diff_birdbaths_hounslow == bird_baths - hounslow)
+    model.AddAbsEquality(1, diff_birdbaths_hounslow)
+
+    # spain and lakes
+    model.Add(lake == spain)
+
+    # finch and squawking
+    model.Add(finch == squawking)
+
+    # croissants and pecking
+    model.Add(croissants == pecking)
+
+    # woodpeckers in tenerife
+    model.Add(woodpecker == canaries)
+
+    # straw, birdbath
+    model.Add(straw == bird_baths)
+
+    # chocolate chip cookies, switzerland
+    model.Add(cookies == switzerland)
+
+    # shiny things, ealing
+    model.Add(stealing == ealing)
+
+    # chimney stack one east of hedge
+    model.Add(chimney == hedge + 1)
+
+    # starling, scones
+    model.Add(starling == scones)
+
+    # hedges, cakes
+    model.Add(hedge == cake)
+
+    # mallard, barking
+    model.Add(mallard == barking)
+
+    # swallow, house
+    model.Add(swallow == house)
+
+    # We want to know everything...
+    solver = cp_model.CpSolver()
+    solved = solver.Solve(model)
+    if solved:
+        print
+        print("nests:", [solver.Value(nests[i]) + 1 for i in range(5)])
+        print("breeds:", [solver.Value(breeds[i]) + 1 for i in range(5)])
+        print("foods:", [solver.Value(foods[i]) + 1 for i in range(5)])
+        print("activities:", [solver.Value(activities[i]) + 1 for i in range(5)])
+        print("hometowns:", [solver.Value(hometowns[i]) + 1 for i in range(5)])
+
+
+if __name__ == "__main__":
+    solve_puzzle()
